7d9357a654c23f9db40d88d5424287c76f613a89
Glavo <zjx001202@gmail.com>
2024-10-07 01:10:49 +0800

    update

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/versions/ModListPageSkin.java
@@ -56,6 +56,7 @@ import org.jackhuang.hmcl.util.StringUtils;
 import org.jackhuang.hmcl.util.i18n.I18n;
 import org.jackhuang.hmcl.util.io.CompressingUtils;
 import org.jackhuang.hmcl.util.io.FileUtils;
+import org.jackhuang.hmcl.util.io.IOUtils;
 import org.jackhuang.hmcl.util.io.NetworkUtils;
 import org.jetbrains.annotations.NotNull;
 
